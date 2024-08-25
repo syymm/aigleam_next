bd428559e9870041236211c8c514544869fdb74b
syymm <shiyuming233@gmail.com>
2024-08-25 14:55:32 +0900

    修改了变量名

--- src/utils/verificationCodeStore.ts
+++ src/utils/verificationCodeStore.ts
@@ -5,21 +5,21 @@ interface VerificationCode {
   
   const codeStore: Map<string, VerificationCode> = new Map();
   
-  export const saveVerificationCode = (username: string, code: string) => {
+  export const saveVerificationCode = (email: string, code: string) => {
     const expiresAt = new Date(Date.now() + 1 * 60 * 1000); // 10 分钟后过期
-    codeStore.set(username, { code, expiresAt });
+    codeStore.set(email, { code, expiresAt });
   };
   
-  export const verifyCode = (username: string, code: string): boolean => {
-    const storedCode = codeStore.get(username);
+  export const verifyCode = (email: string, code: string): boolean => {
+    const storedCode = codeStore.get(email);
     if (!storedCode) return false;
     if (storedCode.expiresAt < new Date()) {
-      codeStore.delete(username);
+      codeStore.delete(email);
       return false;
     }
     return storedCode.code === code;
   };
   
-  export const deleteVerificationCode = (username: string) => {
-    codeStore.delete(username);
+  export const deleteVerificationCode = (email: string) => {
+    codeStore.delete(email);
   };
